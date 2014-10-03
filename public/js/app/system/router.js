6408261446fd9b4171a005f7f6b1582bfef04a15
Arvind Kunday <arvind.kunday@rea-group.com>
2014-10-03 12:06:26 +1000

    Show API version alongside.

--- public/js/app/system/router.js
+++ public/js/app/system/router.js
@@ -1,12 +1,17 @@
-define(['backbone', 'app/system/views/index', 'app/system/models/info'], function(Backbone, IndexView, InfoModel) {
+define(['backbone',
+  'app/system/views/index',
+  'app/system/models/info',
+  'app/system/models/version'], function(Backbone, IndexView, InfoModel, VersionModel) {
   return Backbone.Router.extend({
     routes: {
       "system": "index"
     },
     index: function() {
-      var model = new InfoModel()
-      var view = new IndexView({model: model});
+      var model = new InfoModel();
+      var version = new VersionModel();
+      var view = new IndexView({model: model, version: version});
       model.fetch({reset: true});
+      version.fetch({reset: true});
       $("#panel").html(view.render().el);
     }
   })
--- public/js/app/system/views/index.js
+++ public/js/app/system/views/index.js
@@ -1,12 +1,15 @@
 define(['backbone', "text!app/system/views/templates/index.html"], function(Backbone, template) {
   return Backbone.View.extend({
     template: _.template(template),
-    initialize: function() {
+    initialize: function(options) {
       this.model.bind('change', this.render, this);
       this.model.bind('reset', this.render, this);
+      this.version = options['version'];
+      this.version.bind('change', this.render, this);
+      this.version.bind('reset', this.render, this);
     },
     render: function() {
-      $(this.el).html(this.template({model: this.model}));
+      $(this.el).html(this.template({model: this.model, version: this.version}));
       return this;
     }
   });
