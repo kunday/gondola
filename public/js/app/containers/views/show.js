1a9830b497effee49cd1d2f54675cf640ef1d1fd
Arvind Kunday <arvind.kunday@rea-group.com>
2014-10-04 15:50:18 +1000

    Show container information in tabs.

--- public/js/app/containers/views/show.js
+++ public/js/app/containers/views/show.js
@@ -1,13 +1,30 @@
 define(['backbone', 'text!app/containers/templates/show.html'], function(Backbone, template) {
   return Backbone.View.extend({
     template: _.template(template),
+    events: {
+      'click .module-nav-tabs': 'navigate'
+    },
     initialize: function() {
+      this.model.tabs = ['info', 'state', 'networking', 'logs'];
       this.model.bind('reset', this.render, this);
       this.model.bind('change', this.render, this);
     },
     render: function() {
       $(this.el).html(this.template({model: this.model}));
+      this.toggleView("info");
       return this;
+    },
+    resetActiveLinks: function(currentTarget) {
+      $(this.el).find("li").removeClass("active");
+      $(currentTarget).addClass("active");
+    },
+    toggleView: function(target){
+      $(this.el).find(".module-nav-content").hide();
+      $(this.el).find("#module-nav-content-" + target).show();
+    },
+    navigate: function(e) {
+      this.resetActiveLinks(e.currentTarget);
+      this.toggleView($(e.currentTarget).find("a").data("target"));
     }
   });
 });
