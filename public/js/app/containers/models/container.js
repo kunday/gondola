acb77ec2275cb36832bcaeecdbb8695d975198f5
Arvind Kunday <arvind.kunday@rea-group.com>
2014-10-04 19:42:57 +1000

    Add options for making post requests for containers.

--- public/js/app/containers/models/container.js
+++ public/js/app/containers/models/container.js
@@ -6,6 +6,34 @@ define(['backbone'], function(Backbone){
     urlRoot: '/api/request/containers/',
     url: function() {
       return this.urlRoot + this.containerId + "/json";
+    },
+    start: function() {
+      this._post("start");
+    },
+    stop: function() {
+      this._post("stop");
+    },
+    restart: function() {
+      this._post("restart");
+    },
+    kill: function() {
+      this._post("kill");
+    },
+    pause: function() {
+      this._post("pause");
+    },
+    unpause: function() {
+      this._post("unpause");
+    },
+    remove: function() {
+      this._post("remove");
+    },
+    _post: function(endpoint) {
+      $.ajax({
+        type: 'POST',
+        url: this.urlRoot+this.containerId+"/"+endpoint,
+        async: false
+      });
     }
   });
 });
