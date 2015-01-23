d2d2b1f5c26977eef417f18149173aa9d509bd53
Arvind Kunday <arvind.kunday@rea-group.com>
2015-01-23 12:10:29 +1100

    Fix Api to make calls over ssl.

--- lib/applications/api.rb
+++ lib/applications/api.rb
@@ -1,12 +1,30 @@
-require 'net/http'
+require 'faraday'
+
 class Api < Sinatra::Application
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   get '/request/*' do
     content_type :json
-    return Net::HTTP.get(URI("http://127.0.0.1:2345/#{params[:splat].first}#{options(params)}"))
+    conn.get("#{params[:splat].first}#{options(params)}").body
   end
   private
   def options(params)
     return "" unless params["all"]
     "?all=#{params["all"]}"
+  end
+
+  def conn
+    Faraday.new("https://#{ENV.fetch("DOCKER_HOST")}:#{ENV.fetch("DOCKER_PORT")}", ssl: ssl_options) do |faraday|
+      faraday.adapter :net_http
+      faraday.request :url_encoded
+    end
+  end
+
+  def ssl_options
+    {
+      client_cert: OpenSSL::X509::Certificate.new(ENV.fetch("CERT")),
+      client_key: OpenSSL::PKey::RSA.new(ENV.fetch("KEY")),
+      verify: OpenSSL::SSL::VERIFY_NONE,
+      ca_file: ENV.fetch("CA")
+    }
   end
 end
