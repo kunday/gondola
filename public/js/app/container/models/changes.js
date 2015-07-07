ae824db28bd4c6cb79bbf78c55896cff582ee28c
Arv Kunday <mail@kunday.com>
2015-07-08 00:14:18 +1000

    Show changes to container.

--- public/js/app/container/models/changes.js
+++ public/js/app/container/models/changes.js
@@ -1,4 +1,4 @@
-define(['backbone'], function () {
+define(['backbone'], function (Backbone) {
   return Backbone.Model.extend({
     initialize: function(options) {
       this.containerId = options.id;
