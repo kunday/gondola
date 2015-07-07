1bcff96fdbffb3d657fcb2266ae5346a3dd46bd2
Arv Kunday <mail@kunday.com>
2015-07-08 01:26:39 +1000

    Add views for stats.

--- public/js/app/container/router.js
+++ public/js/app/container/router.js
@@ -1,22 +1,27 @@
 define(['backbone',
   'app/container/models/container',
   'app/container/models/changes',
+  'app/container/models/stats',
   'app/container/models/logs',
   'app/container/views/show',
   'app/container/views/changes',
+  'app/container/views/stats',
   'app/container/views/logs'],
   function(Backbone,
     ContainerInfo,
     ContainerChanges,
+    ContainerStats,
     ContainerLogs,
     ContainerInfoView,
     ContainerChangesView,
+    ContainerStatsView,
     ContainerLogsView) {
     return Backbone.Router.extend({
       routes: {
         'container/show/:id': 'show',
         'container/changes/:id': 'changes',
-        'container/logs/:id': 'logs'
+        'container/logs/:id': 'logs',
+        'container/stats/:id': 'stats'
       },
       show: function (id) {
         var container = new ContainerInfo({"id": id});
@@ -34,6 +39,12 @@ define(['backbone',
         var changes = new ContainerLogs({"id": id});
         var view = new ContainerLogsView({model: changes});
         changes.fetch({reset: true});
+        $("#panel").html(view.render().el);
+      },
+      stats: function(id) {
+        var stats = new ContainerStats({"id": id});
+        var view = new ContainerStatsView({model: stats});
+        stats.fetch({reset: true});
         $("#panel").html(view.render().el);
       }
     });
---
+++ public/js/app/container/views/stats.js
@@ -0,0 +1,16 @@
+define(['backbone',
+  'text!app/container/templates/stats.html'],
+  function(Backbone, template) {
+    return Backbone.View.extend({
+      template: _.template(template),
+      initialize: function(){
+        this.model.bind('change', this.render, this);
+        this.model.bind('reset', this.render, this);
+      },
+      render: function(e) {
+        console.log(this.model.attributes);
+        $(this.el).html(this.template({model: this.model}));
+        return this;
+      }
+    });
+});
