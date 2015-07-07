81515d0053b55bd192306b32ce2ca9430a6a499b
Arv Kunday <mail@kunday.com>
2015-07-08 02:07:45 +1000

    Fix the unable to load logs problem.

--- lib/applications/api.rb
+++ lib/applications/api.rb
@@ -1,15 +1,10 @@
 class Api < Sinatra::Application
   get '/request/*' do
     content_type :json
-    connection.request(:get, "#{params[:splat].first}#{options(params)}")
+    connection.request(:get, "#{params[:splat].first}?#{request.query_string}")
   end
 
   private
-
-  def options(params)
-    return '' unless params['all']
-    "?all=#{params['all']}"
-  end
 
   def connection
     Docker::Connection.new(Environment.docker_url)
