c05a752def9be28895da6e465ea8b8dd2211f5ff
Arv Kunday <mail@kunday.com>
2015-07-07 21:57:17 +1000

    Modulize the images view.

--- public/js/app/images/router.js
+++ public/js/app/images/router.js
@@ -1,29 +1,43 @@
 define(['backbone',
+  'app/shared/views/module',
   'app/images/collections/images',
   'app/images/views/index',
   'app/images/collections/history',
   'app/images/views/history',
   'app/images/models/image'],
-  function(Backbone, ImagesCollection, ImagesView, HistoryCollection, HistoryView, Image) {
+  function(Backbone, Module, ImagesCollection, ImagesView, HistoryCollection, HistoryView, Image) {
   return Backbone.Router.extend({
+    moduleInitialize: function(subNav){
+      var appModule = new Module({
+        title: "Images",
+        name: "images",
+        router: this,
+        default: subNav
+      });
+      $("#panel").html(appModule.render().el);
+      return appModule;
+    },
     routes: {
       "images": "list",
+      "images/list": "list",
       "images/history/:id": "history",
       "images/delete/:id": "delete"
     },
     list: function () {
+      var module = this.moduleInitialize('list');
       var collection = new ImagesCollection();
       var view = new ImagesView({model: collection});
       collection.fetch({reset: true});
-      $("#panel").html(view.render().el);
+      module.content(view.render().el);
     },
     history: function (id) {
+      var module = this.moduleInitialize('history');
       var historyCollection = new HistoryCollection({"imageId": id});
       var image = new Image({"imageId": id});
       var view = new HistoryView({model: historyCollection, info: image});
       historyCollection.fetch({reset: true});
       image.fetch();
-      $("#panel").html(view.render().el);
+      module.content(view.render().el);
     },
     delete: function (imageId) {
       $("#panel").html("delete image");
